ca0c02d9c5374f607249388621b7884031c92278
paulo-carvalho <paulocarvalho@dcc.ufmg.br>
2015-05-03 21:22:56 -0400

    Initial commit

---
+++ app/src/main/java/android/lehman/sillektis/MainFragment.java
@@ -0,0 +1,11 @@
+package android.lehman.sillektis;
+
+import android.app.Fragment;
+
+/**
+ * Class from Sillektis at android.lehman.sillektis
+ * Created by Paulo-Lehman on 5/3/2015.
+ */
+public class MainFragment extends Fragment {
+
+}
